8f1e406e627925234c1279de9140be2712aaa6eb
Popnenson <livinusbal@gmail.com>
2022-12-12 00:03:41 -0800

    5 prints

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - return single integer from 0-10
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+
+		printf("%d", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
