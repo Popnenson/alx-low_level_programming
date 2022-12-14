4d7efec40db7b8e0678b22141187f770ef3295a9
Popnenson <livinusbal@gmail.com>
2022-12-14 14:15:19 -0800

    It is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,7 @@
-nclude "main.h"
+#include "main.h"
 
 /**
- * _isalpha - Shows 1 if the input is a
- * letter Another cases, shows 0
- *
+ * _isalpha - Shows 1 if the input is a letter Another cases, shows 0
  * @c: The character in ASCII code
  *
  * Return: 1 for letters. 0 for the
