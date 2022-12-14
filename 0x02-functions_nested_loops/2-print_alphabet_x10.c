2c0c3c04600a5ac1fccb3cc6789ea421ba6ee43b
Popnenson <livinusbal@gmail.com>
2022-12-14 07:45:40 -0800

    print aplhabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabets in lower case
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+
+	char alphabet;
+	int num = 1;
+
+	while (num <= 10)
+	{
+
+		alphabet = 'a';
+		while (alphabet <= 'z')
+		{
+
+			_putchar(alphabet);
+			alphabet++;
+		}
+		_putchar('\n');
+		num++;
+	}
+}
